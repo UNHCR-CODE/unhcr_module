91c52ff28b22018662ec702e0bda998daa96d2b2
shermes641 <stevehermes@gmail.com>
2025-01-06 17:34:52 -0600

    Use correct python because Windows scheduler used a different one

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 REM change to your venv path
 call E:\_UNHCR\CODE\unhcr_module\.venv\Scripts\activate.bat
-python E:\_UNHCR\CODE\unhcr_module\unhcr\full_test.py --log INFO
+REM changed python path to make it work in Windows scheduler -- was running a different python
+E:\_UNHCR\CODE\unhcr_module\.venv\Scripts\python.exe E:\_UNHCR\CODE\unhcr_module\unhcr\full_test.py --log INFO
